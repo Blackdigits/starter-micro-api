0d401439d23436c6e9f67a60239d5d6b7f78438f
Blackdigits <galaxian125@gmail.com>
2022-11-30 14:53:26 +0700

    del

--- routes/auth.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const authController = require("../controllers/auth");
-
-router.post("/register", authController.registerUser);
-router.post("/login", authController.loginUser);
-
-module.exports = router;
