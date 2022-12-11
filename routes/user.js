e91036dceec6c60ed4f06cdb8bd231928b61e95f
Blackdigits <galaxian125@gmail.com>
2022-12-11 11:51:07 +0700

    rezero

--- routes/user.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const userController = require("../controllers/user");
-
-router.get("/", userController.getAllUser);
-router.put("/:id", userController.updateUser);
-router.delete("/:id", userController.deleteUser);
-
-module.exports = router;
